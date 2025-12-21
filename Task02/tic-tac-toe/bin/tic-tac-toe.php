9c9956369a51d4a032905ae8f45255fcf948e339
SalihAbdrashitov <ys.salih2004@gmail.com>
2025-12-22 01:40:52 +0300

    did Task02

---
+++ Task02/tic-tac-toe/bin/tic-tac-toe.php
@@ -0,0 +1,5 @@
+#!/usr/bin/env php
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+\kitsu185\TicTacToe\Controller\startGame();
---
+++ Task02/tic-tac-toe/src/Controller.php
@@ -0,0 +1,8 @@
+<?php
+namespace kitsu185\TicTacToe\Controller;
+
+use kitsu185\TicTacToe\View\View;
+
+function startGame() {
+    View::renderStartScreen();
+}
